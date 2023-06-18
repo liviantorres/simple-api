5f195b9080ee8e08b9a710fa99fbf806d73ce2bd
Livian  Torres <liviantorres63@gmail.com>
2023-06-18 08:33:48 -0300

    Spring Boot project

---
+++ src/test/java/com/backend/simpleapi/SimpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
